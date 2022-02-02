d9c22fa45c28d7d04f447496453f143d6760659a
Sevda <sevda.amini@gmail.com>
2022-02-02 21:38:09 +1000

    custom 404 page created

---
+++ pages/404.js
@@ -0,0 +1,19 @@
+import Layout from "../components/Layout";
+import Link from "next/link";
+import styles from "../styles/404.module.css";
+// import { FaExclamationTriangle } from "react-icons/";
+
+export default function PageNotFound() {
+  return (
+    <Layout title="Page Not Found">
+      <div className={styles.error}>
+        <h1>
+          {/* <FaExclamationTriangle /> */}
+          404
+        </h1>
+        <h4>Sorry, there is nothing to show</h4>
+        <Link href="./">Go Back Home</Link>
+      </div>
+    </Layout>
+  );
+}
