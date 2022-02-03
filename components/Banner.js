92da9ef57bfe732872da195865f415ec7e36b893
Sevda <sevda.amini@gmail.com>
2022-02-03 21:07:00 +1000

    banner added

---
+++ components/Banner.js
@@ -0,0 +1,9 @@
+import styles from '@/styles/Banner.module.css'
+export default function Banner() {
+  return (
+      <div className={styles.banner}>
+          <h1>Welcome to the world of art</h1>
+          <h2>Find the art events around you</h2>
+      </div>
+  )
+}
