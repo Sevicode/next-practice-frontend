7bef9c814cc89e111149f5e54e6fa678202d6540
Sevda <sevda.amini@gmail.com>
2022-02-04 23:23:23 +1000

    bug fixed

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,23 @@
 import Layout from "@/components/Layout"
 import Link from "next/link"
 import {API_URL} from "@/config/index"
-import EventItem from "@/components/EventItem"
+import EventItem from '@/components/EventItem'
 
 export default function Home({events}) {
-  console.log(events)
   return (
     <Layout>
-       {events.map((evt) => (
-        <EventItem key={evt.id} evt={evt} />  
-  ))}
+      <h1>Upcoming Events</h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
+
+      {events.length > 0 && (
+        <Link href='/events'>
+          <a className='btn-secondary'>View All Events</a>
+        </Link>
+      )}
     </Layout>
   )
 }
