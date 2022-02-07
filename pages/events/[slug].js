35aa25e344822735583119191acbb9fe2a19218c
Sevda <sevda.amini@gmail.com>
2022-02-07 15:51:43 +1000

    slugs working

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,8 +1,22 @@
 import Layout from "@/components/Layout";
-export default function EventPage() {
+import { API_URL } from "@/config/index";
+export default function EventPage({evt}) {
   return (
     <Layout>
-      <h1>My Event</h1>
+      <h1>{evt.name}</h1>
     </Layout>
   );
+}
+
+
+
+export async function getServerSideProps({ query: {slug}}) {
+  const res = await fetch (`${API_URL}/api/events?slug=${slug}`)
+  const events = await res.json()
+
+  return {
+    props: {
+      evt: events[0]
+    },
+  }
 }
