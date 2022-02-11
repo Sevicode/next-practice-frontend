9ff25c0d8afabf612cfb0dd250d544c1fd60f7c9
Sevda <sevda.amini@gmail.com>
2022-02-11 12:35:23 +1000

    editing

--- components/EventItem.js
+++ components/EventItem.js
@@ -1,17 +1,13 @@
-import Link from 'next/link'
-import Image from 'next/image'
-import styles from '@/styles/EventItem.module.css'
+import Link from "next/link";
+import Image from "next/image";
+import styles from "@/styles/EventItem.module.css";
 
 export default function EventItem({ evt }) {
   return (
     <div className={styles.event}>
       <div className={styles.img}>
         <Image
-          src={
-            evt.image
-              ? evt.image
-              : '/images/default.jpg'
-          }
+          src={evt.image ? evt.image : "/images/default.jpg"}
           alt={evt.title}
           width={170}
           height={100}
@@ -20,16 +16,16 @@ export default function EventItem({ evt }) {
 
       <div className={styles.info}>
         <span>
-          {new Date(evt.date).toLocaleDateString('en-US')} at {evt.time}
+          {new Date(evt.date).toLocaleDateString("en-US")} at {evt.time}
         </span>
         <h3>{evt.name}</h3>
       </div>
 
       <div className={styles.link}>
         <Link href={`/events/${evt.slug}`}>
-          <a className='btn-primary'>Details</a>
+          <a className="btn-primary">Details</a>
         </Link>
       </div>
     </div>
-  )
+  );
 }
--- pages/api/events/index.js
+++ pages/api/events/index.js
@@ -1,14 +1,12 @@
-import { parseBody } from 'next/dist/server/api-utils'
+import { parseBody } from "next/dist/server/api-utils";
 
-const { events } = require('./data.json')
+const { events } = require("./data.json");
 
-// eslint-disable-next-line import/no-anonymous-default-export
 export default (req, res) => {
-  if(req.method === 'GET') {
-    res.status(200).json(events)
-  }else {
-    res.setHeader('Allow', [GET])
-    res.status(405).json({message: `Method ${req.method} is not allowed`})
-
+  if (req.method === "GET") {
+    res.status(200).json(events);
+  } else {
+    res.setHeader("Allow", [GET]);
+    res.status(405).json({ message: `Method ${req.method} is not allowed` });
   }
-}
+};
